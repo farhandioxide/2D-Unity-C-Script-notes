d7f33d19030df609de3ea8d73aa68c40a62f56f8
Farhan Zaman <farhanzaman314@gmail.com>
2026-03-13 01:59:02 +0600

    Implement player movement and jumping mechanics
    created first note codes for 2D movements and jump

---
+++ Player Movements.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    //var
+
+    public float speed = 6f;
+    public float jumpspeed = 15f;
+    private bool isGround = true;
+
+    //ref
+
+    Rigidbody2D rb;
+
+    //start
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    //start
+
+    void Update()
+    {
+
+        Move();
+        Jump();
+
+
+    }
+
+    //fixedupdates
+
+    void FixedUpdate()
+    {
+
+        
+
+    }
+
+    //move left right
+
+    private void Move()
+    {
+        float move = 0;
+        if (Keyboard.current.dKey.isPressed)
+        {
+            move = 1;
+            
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            move = -1;
+            
+        }
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+    }
+
+    // jump up
+
+
+    private void Jump()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGround)
+        {
+         
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpspeed);
+        }
+    }
+
+    //ground check
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGround = false;
+    }
+
+
+}
